9948810780cc3869fbeec4768fb4af0adff8717c
Dava didat <davadidatqomaladi@gmail.com>
2025-11-10 07:53:03 +0700

    ucapan ultah

---
+++ scrip.js
@@ -0,0 +1,7 @@
+document.querySelector("#btn1").addEventListener("click", (e) => {
+  document.querySelector("#dialog1").showModal();
+});
+
+document.querySelector("#btn2").addEventListener("click", (e) => {
+  document.querySelector("#dialog2").showModal();
+});
